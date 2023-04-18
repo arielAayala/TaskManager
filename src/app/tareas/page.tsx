5b77e22be1d7c859c46b078907f2c757e07c1161
Santi Handel <santiagohandel@gmail.com>
2023-04-18 03:00:24 -0300

    mas cositas

--- src/app/tareas/page.tsx
+++ src/app/tareas/page.tsx
@@ -3,13 +3,14 @@ import { useRouter } from "next/navigation";
 import React from "react";
 import {lstencargo} from "../../utils/encargo.ts"
 import Encargo from "../../components/encargo/encargo"
+
 function Tareas() {
 	return(
 		<div>
 			<h1>Tareas</h1>
 			<div>
 				{lstencargo.map(i=>{
-					return <Encargo key={i.idEncargo} descripcionEncargo={i.descripcionEncargo}></Encargo>
+					return <Encargo key={i.idEncargo} idEncargo={i.idEncargo} idUsuarioCreador={i.idUsuarioCreador} descripcionEncargo={i.descripcionEncargo}></Encargo>
 				})}
 			</div>
 		</div>
--- src/components/encargo/encargo.tsx
+++ src/components/encargo/encargo.tsx
@@ -1,10 +1,19 @@
 import React from "react";
 import { Encargo } from "./encargo.models";
+import style from "./encargo.module.css"
 
 function Encargo(props:Encargo) {
+
+    const handleClick=()=>{
+        console.log();
+        
+    }
+
 	return (
-        <div>
-            <h4 className="style.encargo"> {props.descripcionEncargo} </h4>
+        <div className={style.container} onClick={handleClick}>
+            <h4> {props.descripcionEncargo} </h4>
+            <h4> {props.idUsuarioCreador} </h4>
+            <h4> {props.idEncargo} </h4>
         </div>
     )
 }
