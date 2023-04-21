d43cd5a4f2c6647dcf2f39c9d696d1595a25a94c
Santi Handel <santiagohandel@gmail.com>
2023-04-21 16:20:11 -0300

    algunos cambios, se sigue rompiendo

--- src/components/encargo/encargo.tsx
+++ src/components/encargo/encargo.tsx
@@ -1,21 +1,20 @@
 import React from "react";
 import { Encargo } from "./encargo.models";
 import style from "./encargo.module.css";
-import  Popup  from "../popups/popup";
-function Encargo(props:Encargo) {
-
-   const clickHandle=()=>{
-    console.log("estoy tilteadisimo");  
-   }
+import Popup from "../popups/popup";
+function Encargo(props: Encargo) {
+	const clickHandle = () => {
+		console.log("estoy tilteadisimo");
+	};
 
 	return (
-        <div className={style.container} onClick={clickHandle}>
-            <h4> {props.descripcionEncargo} </h4>
-            <h4> {props.idUsuarioCreador} </h4>
-            <h4> {props.idEncargo} </h4> 
-            <Popup></Popup>
-        </div>
-    )
+		<div className={style.container} onClick={clickHandle}>
+			<h4> {props.descripcionEncargo} </h4>
+			<h4> {props.idUsuarioCreador} </h4>
+			<h4> {props.idEncargo} </h4>
+			<Popup></Popup> ; 
+		</div>
+	);
 }
 
 export default Encargo;
