1268a22aa944d01dbd96197ead61971a07e44800
Fabricio Valdez <100192392+fasit0v@users.noreply.github.com>
2023-04-23 10:54:53 -0300

    index

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
-import Navbar from "@/components/navbar/navbar";
 import "./global.css";
+import Link from 'next/link';
+import Head from 'next/head';
 import Footer from "@/components/footer/footer";
+import Navbar from "@/components/navbar/navbar";
 
 export default function RootLayout({
 	children,
@@ -9,6 +11,11 @@ export default function RootLayout({
 }) {
 	return (
 		<html lang="en">
+				<Head>
+				<title> Setic </title>
+				<meta name="description" content="Generate by create next app"></meta>
+				<Link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+				</Head>
 			<body>
 				<Navbar></Navbar>
 				<div className="containerChildrens">{children}</div>
