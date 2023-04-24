5628c1d82b2c774117c3f6b311484ef18a209b24
Fabricio Valdez <100192392+fasit0v@users.noreply.github.com>
2023-04-24 05:12:43 -0300

    cambios

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,4 @@
 import "./global.css";
-import Link from 'next/link';
-import Head from 'next/head';
 import Footer from "@/components/footer/footer";
 import Navbar from "@/components/navbar/navbar";
 
@@ -11,11 +9,6 @@ export default function RootLayout({
 }) {
 	return (
 		<html lang="en">
-				<Head>
-				<title> Setic </title>
-				<meta name="description" content="Generate by create next app"></meta>
-				<Link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
-				</Head>
 			<body>
 				<Navbar></Navbar>
 				<div className="containerChildrens">{children}</div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,17 @@
 import React from "react";
-import style from "./home.module.css";
+import styles from "./home.module.css";
 
-export default function Home() {
+export const metadata = {
+	title: 'Setic',
+  };
+
+  export default function Home() {
 	return (
 		<div>
 			<h1> Bienvenido </h1>
 			<h2> SeTIC </h2>
-			<p className={style.text}> El Servicio Técnico Interdisciplinario Central (Se.T.I.C.), es un equipo de apoyo y orientación dependiente del Ministerio de Cultura y Educación de la Provincia de Formosa, transversal a todo el Sistema Educativo Provincial. Atiende desde el Nivel Inicial hasta el Nivel Superior y sus modalidades, contribuyendo al mejoramiento de la calidad educativa, los climas institucionales y el fortalecimiento de vínculos, a través de un abordaje institucional e integral. Su accionar se enmarca en la Resolución N°2.692 del Ministerio de Cultura y Educación de la Provincia de Formosa y en Resolución Nacional CFE Nº 239/14 anexo II. </p>
+			<span className={styles.material_symbols_outlined}> search </span>
+			<p className={styles.text}> El Servicio Técnico Interdisciplinario Central (Se.T.I.C.), es un equipo de apoyo y orientación dependiente del Ministerio de Cultura y Educación de la Provincia de Formosa, transversal a todo el Sistema Educativo Provincial. Atiende desde el Nivel Inicial hasta el Nivel Superior y sus modalidades, contribuyendo al mejoramiento de la calidad educativa, los climas institucionales y el fortalecimiento de vínculos, a través de un abordaje institucional e integral. Su accionar se enmarca en la Resolución N°2.692 del Ministerio de Cultura y Educación de la Provincia de Formosa y en Resolución Nacional CFE Nº 239/14 anexo II. </p>
 		</div>
 	);
 }
